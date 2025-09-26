39311a5dfddc07e1565c38b15f75c7c477b6f90d
Cecília Moura Lira <mariaceciliamouralira@hotmail.com>
2025-09-26 10:07:19 -0300

    Add files via upload

---
+++ Desafio 1/Desafio1.js
@@ -0,0 +1,9 @@
+//Peça ao usuário dois números e exiba a soma deles.
+let numero1= parseFloat(prompt("Digite o primeiro número:"));
+let numero2= parseFloat(prompt("Digite o segundo número:"));
+let soma= numero1 + numero2;
+
+alert("A soma dos dois números é: " + soma);
+
+//Se usarmos console.log o resultado da operação será exibido no console
+// Com o Alert o resultado vai ser exibido como alerta no navegador
